626048fc20c84d6cad199577b421b8e6f3f230af
TalatCikikci <talat.cikikci@gmail.com>
2016-10-25 10:36:18 +0300

    Created Basic Login and Signup Pages
    login.html and signup.html created

--- fitster/healthtracker/views.py
+++ fitster/healthtracker/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World this is the fitster home page!")
+    return render(request, 'healthtracker/login.html')
 
 def signup(request):
-    return HttpResponse("Hello World this is the fitster signup page!")
+    return render(request, 'healthtracker/signup.html')
