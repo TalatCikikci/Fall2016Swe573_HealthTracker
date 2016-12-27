b128b5602bcc1581ae6c44f335256c1ec3c62b66
TalatCikikci <talat.cikikci@gmail.com>
2016-12-27 17:44:27 +0300

    Unit Test for Signup Added
    A unit test to check if only one userprofile object is created during
    signup has been implemented.

--- fitster/fitster/sql/createdb.sql
+++ fitster/fitster/sql/createdb.sql
@@ -3,3 +3,4 @@ CREATE USER 'fitsterdb'@'localhost' IDENTIFIED BY 'fitsterdb';
       DEFAULT CHARACTER SET utf8
       DEFAULT COLLATE utf8_general_ci;
     GRANT ALL ON fitsterdb.* TO 'fitsterdb'@'localhost';
+	GRANT ALL ON test_fitsterdb.* TO 'fitsterdb'@'localhost';
--- fitster/healthtracker/tests.py
+++ fitster/healthtracker/tests.py
@@ -1,6 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, BaseUserManager
 
-# Create your tests here.
-#class SignupTests(TestCase):
-#    
-#    def test_
+import datetime
+
+from .models import Userprofile
+
+import healthtracker.signals as signals
+
+class SignupTests(TestCase):
+
+    def test_only_one_userprofile_data_submitted_during_signup(self):
+        email = BaseUserManager.normalize_email("talat.cikikci@gmail.com")
+        djangouser = User.objects.create_user("talatusername",
+                                              email,
+                                              "talatpassword")
+        djangouser.last_name = "Cikikci"
+        djangouser.first_name = "Talat"
+        djangouser._dateofbirth = datetime.date.today()
+        djangouser._gender = "M"
+        djangouser._height = "182"
+        djangouser._weight = "80"
+        djangouser._notes = "User's notes."
+        
+        signals.user_initiated.send(sender=None, 
+                            instance=djangouser,
+                            dateofbirth=djangouser._dateofbirth,
+                            gender=djangouser._gender,
+                            height=djangouser._height,
+                            weight=djangouser._weight,
+                            notes=djangouser._notes)
+        
+        djangouser.save()
+        
+        created_user = User.objects.get(username="talatusername")
+        user_entries = Userprofile.objects.filter(user_id=created_user.id)
+        
+        self.assertEquals(len(user_entries),1)
